d51edb624685ba8f74809d3c34fab7dbe0a81b74
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-04-05 16:05:11 +0300

    add js file

--- app.js
+++ app.js
@@ -22,16 +22,14 @@ while (robotTest !== '5') {
 }
 
 
-function printImages () {
-for (var A=1; A<3; A++) {
-    document.write ('<img style="width: 500px;" src="https://assets.materialup.com/uploads/9993c23c-53db-4ff3-8b0c-aad9d001edf3/preview.png" />');
-    document.write (A);
-}
-var userInput = prompt ('please enter the number of a blank pages that you need');
-for (var B=1 ; B <= userInput ; B++) {
-    document.write ('<img style="width: 300px;" src="https://i.pinimg.com/originals/d3/38/77/d338774cd740e03069eff6a2f94e1f4a.png" />');
-    document.write (B);
-   }
+function printImages (pram1 , image) {
+    for (var A=1; A<=pram1; A++) {
+        document.write (image);
+        document.write (A);
+    }     
 }
 
-printImages ();
+var userInput = prompt ('please enter the number of a blank pages that you need');
+
+printImages (3,'<img style="width: 300px;" src="https://assets.materialup.com/uploads/9993c23c-53db-4ff3-8b0c-aad9d001edf3/preview.png" />' );
+printImages (userInput, '<img style="width: 300px;" src="https://i.pinimg.com/originals/d3/38/77/d338774cd740e03069eff6a2f94e1f4a.png" />' );
