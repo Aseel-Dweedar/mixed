fe40a71c1314d996f96bb7f36c35a1c25709e14e
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-04-07 18:36:26 +0300

    add ex1 file

--- app.js
+++ app.js
@@ -16,20 +16,20 @@
 // }
 
 
-var robotTest = prompt ('Please enter 5 to make sure you are not a robot');
-while (robotTest !== '5') {
-    robotTest = prompt ('Please enter 5 ');
+// var robotTest = prompt('Please enter 5 to make sure you are not a robot');
+// while (robotTest !== '5') {
+//     robotTest = prompt('Please enter 5 ');
+// }
+
+
+function printImages(pram1, image) {
+    for (var A = 1; A <= pram1; A++) {
+        document.write(image);
+        document.write(A);
+    }
 }
 
+// var userInput = prompt('please enter the number of a blank pages that you need');
 
-function printImages (pram1 , image) {
-    for (var A=1; A<=pram1; A++) {
-        document.write (image);
-        document.write (A);
-    }     
-}
-
-var userInput = prompt ('please enter the number of a blank pages that you need');
-
-printImages (3,'<img style="width: 300px;" src="https://assets.materialup.com/uploads/9993c23c-53db-4ff3-8b0c-aad9d001edf3/preview.png" />' );
-printImages (userInput, '<img style="width: 300px;" src="https://i.pinimg.com/originals/d3/38/77/d338774cd740e03069eff6a2f94e1f4a.png" />' );
+// printImages (3,'<img style="width: 300px;" src="https://assets.materialup.com/uploads/9993c23c-53db-4ff3-8b0c-aad9d001edf3/preview.png" />' );
+// printImages (userInput, '<img style="width: 300px;" src="https://i.pinimg.com/originals/d3/38/77/d338774cd740e03069eff6a2f94e1f4a.png" />' );
