7aaa42d3f509655be410eeb7b90c1f426e397e8d
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-03 21:46:12 +0300

    renaming

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// alert ('Hello');
-// var c = confirm('lets play a game');
+alert('Hello');
+var c = confirm('lets play a game');
 
-// if (c == true) {
-//     var age = prompt ('enter your age');
-// } else {
-//     alert ('then, see next time');
-// }
+if (c == true) {
+    var age = prompt('enter your age');
+} else {
+    alert('then, see next time');
+}
 
-// if (age >= 35) {
-//     document.write ('group1');
-// } else if (age < 35) {
-//     document.write ('group2');
-// } else {
-//     document.write ('please enter a number');
-// }
+if (age >= 35) {
+    document.write('group1');
+} else if (age < 35) {
+    document.write('group2');
+} else {
+    document.write('please enter a number');
+}
 
 
-// var robotTest = prompt('Please enter 5 to make sure you are not a robot');
-// while (robotTest !== '5') {
-//     robotTest = prompt('Please enter 5 ');
-// }
+var robotTest = prompt('Please enter 5 to make sure you are not a robot');
+while (robotTest !== '5') {
+    robotTest = prompt('Please enter 5 ');
+}
 
 
 function printImages(pram1, image) {
@@ -29,7 +29,7 @@ function printImages(pram1, image) {
     }
 }
 
-// var userInput = prompt('please enter the number of a blank pages that you need');
+var userInput = prompt('please enter the number of a blank pages that you need');
 
-// printImages (3,'<img style="width: 300px;" src="https://assets.materialup.com/uploads/9993c23c-53db-4ff3-8b0c-aad9d001edf3/preview.png" />' );
-// printImages (userInput, '<img style="width: 300px;" src="https://i.pinimg.com/originals/d3/38/77/d338774cd740e03069eff6a2f94e1f4a.png" />' );
+printImages(3, '<img style="width: 300px;" src="https://assets.materialup.com/uploads/9993c23c-53db-4ff3-8b0c-aad9d001edf3/preview.png" />');
+printImages(userInput, '<img style="width: 300px;" src="https://i.pinimg.com/originals/d3/38/77/d338774cd740e03069eff6a2f94e1f4a.png" />');
